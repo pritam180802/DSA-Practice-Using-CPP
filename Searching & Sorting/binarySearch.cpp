a485cd9a7270bdba5d0b2fe257f3f25ecac4fbee
Pritam Kayal <kayalpritam65@gmail.com>
2026-03-21 23:10:34 +0530

    Binary Search

---
+++ Searching & Sorting/binarySearch.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+bool bSearch(int start,int end,int key,int arr[]){
+    while(start <= end){
+       int mid = (start+end)/2;
+       if(arr[mid] == key)
+         return true;
+        else if(arr[mid]<key)
+          start = mid+1;
+        else 
+          end = mid-1;
+    }
+    return false;
+}
+
+int main(){
+    int size;
+    cout<<"Enter the size of Array: ";
+    cin>>size;
+    int arr[size];
+    for(int i=0;i<size;i++){
+        cout<<"Enter the "<<i+1<<"'s element: ";
+        cin>>arr[i];
+    }
+    int key;
+    cout<<"Enter a elememt to search: ";
+    cin>>key;
+    if(bSearch(0,size-1,key,arr))
+    cout<<"Present";
+    else
+    cout<<"Not Present!!!!";
+}
